a85c441bf11a6c154ee4a89540d4a21dc4d280ce
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-11-27 20:21:35 -0300

    Constructor

--- app/src/main/java/br/com/etecia/myapp/Cars.java
+++ app/src/main/java/br/com/etecia/myapp/Cars.java
@@ -4,4 +4,24 @@ public class Cars {
 
     //Varivaies Globais
     private String titulo;
+    private int imgCars;
+
+    public Cars() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImgCars() {
+        return imgCars;
+    }
+
+    public void setImgCars(int imgCars) {
+        this.imgCars = imgCars;
+    }
 }
