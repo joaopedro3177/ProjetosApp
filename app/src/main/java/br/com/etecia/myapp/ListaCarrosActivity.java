1a414e9f434a2637d483f0be29f672605d289cbc
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-12-04 21:32:10 -0300

    Array

--- app/src/main/java/br/com/etecia/myapp/ListaCarrosActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaCarrosActivity.java
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListaCarrosActivity extends AppCompatActivity {
@@ -25,9 +26,15 @@ public class ListaCarrosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //criando um array de dados
+        lstCars = new ArrayList<>();
+        lstCars.add();
+
         //RecycleView
         listaCarros = findViewById(R.id.listaCarros);
+
         CarAdapter adapter = new CarAdapter(lstCars, ListaCarrosActivity.this);
+
         //Liga o adaptador a lista
         listaCarros.setAdapter(adapter);
     }
